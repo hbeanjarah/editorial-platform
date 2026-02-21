59ba1a54d6db122d648731c9dfb649ac81190704
hbeanjarah <hbeanjarahoussen@gmail.com>
2026-02-21 15:11:40 +0300

    chore: deploy application to cloud

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -3,7 +3,8 @@ import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL ?? "",
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const prisma = new PrismaClient({ adapter });
