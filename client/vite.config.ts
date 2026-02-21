f91beba2a3cba6760639f3ac85460fba4fa20279
hbeanjarah <hbeanjarahoussen@gmail.com>
2026-02-21 15:27:26 +0300

    chore: get api url from env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3001",
+      "/api": process.env.VITE_API_URL || "http://localhost:3001",
     },
   },
 });
