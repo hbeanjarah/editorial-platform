c86204b20d3f8f7e97f230fb8b345ee020344901
hbeanjarah <hbeanjarahoussen@gmail.com>
2026-02-21 15:51:45 +0300

    chore: add remove api url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "/api"
+    : `${import.meta.env.VITE_API_URL}/api`;
+
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -13,7 +12,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": process.env.VITE_API_URL || "http://localhost:3001",
+      "/api": "http://localhost:3001",
     },
   },
 });
